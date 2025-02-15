08e9a3f4104e9cb2a96c2abc03264fd9b11d3881
Gosuke Miyashita <gosukenator@gmail.com>
2025-02-15 19:04:39 +0900

    Update

--- config.ts
+++ config.ts
@@ -1,17 +1,27 @@
-import { HotelLocation } from "./config_type";
-import type { Configs } from "./config_type";
+import {HotelLocation} from "./config_type";
+import type {Configs} from "./config_type";
 
 const configs: Configs = [
-  {
-    location: HotelLocation.Odaiba,
-    arrivalDate: new Date("2025-03-09"),
-    departureDate: new Date("2025-03-10"),
-  },
-  {
-    location: HotelLocation.Ariake,
-    arrivalDate: new Date("2025-03-09"),
-    departureDate: new Date("2025-03-10"),
-  },
+    {
+        location: HotelLocation.Ariake,
+        arrivalDate: new Date("2025-11-12"),
+        departureDate: new Date("2025-11-14"),
+    },
+    {
+        location: HotelLocation.Fukuoka,
+        arrivalDate: new Date("2025-11-13"),
+        departureDate: new Date("2025-11-14"),
+    },
+    {
+        location: HotelLocation.Ariake,
+        arrivalDate: new Date("2025-11-16"),
+        departureDate: new Date("2025-11-17"),
+    },
+    {
+        location: HotelLocation.Ariake,
+        arrivalDate: new Date("2025-11-16"),
+        departureDate: new Date("2025-11-18"),
+    },
 ];
 
-export { configs };
+export {configs};
