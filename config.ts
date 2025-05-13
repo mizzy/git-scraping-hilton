fe97934251bbef8351c72dd052e64ad810c861a5
Gosuke Miyashita <gosukenator@gmail.com>
2025-05-13 19:13:13 +0900

    Update

--- config.ts
+++ config.ts
@@ -3,6 +3,11 @@ import type { Configs } from "./config_type";
 
 const configs: Configs = [
   {
+    location: HotelLocation.Ariake,
+    arrivalDate: new Date("2025-07-11"),
+    departureDate: new Date("2025-07-13"),
+  },
+  {
     location: HotelLocation.Fukuoka,
     arrivalDate: new Date("2025-11-12"),
     departureDate: new Date("2025-11-13"),
